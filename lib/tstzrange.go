6e2dca40cf8892a9c6bcc7a98785e63a6588fb41
Sumit Asok <sumitasok@gmail.com>
2021-09-06 11:27:26 +0530

    removing type from var declaration globally.

--- lib/tstzrange.go
+++ lib/tstzrange.go
@@ -16,7 +16,7 @@ type TstzrangeI interface {
 }
 
 //var _ TstzrangeI = Tstzrange{}
-var timeFormat string = "2006-01-02 15:04:05-07:00"
+var timeFormat = "2006-01-02 15:04:05-07:00"
 
 func NewTstzrange(prefix rune, fromTime, toTime time.Time, postfix rune) (*Tstzrange, error) {
 	return &Tstzrange{prefix: prefix, FromTime: fromTime, ToTime: toTime, postfix: postfix}, nil
